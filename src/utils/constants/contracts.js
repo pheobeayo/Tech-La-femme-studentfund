aa6ae2788bc39df340078ac58875d0e2b1bbf85b
okste1234 <okste1234@gmail.com>
2024-04-16 00:56:53 +0100

    FEAT: add proper contract addresses to contracts instance

--- src/utils/constants/contracts.js
+++ src/utils/constants/contracts.js
@@ -5,14 +5,14 @@ import Abi2 from "./fundStudent.json";
 
 export const getStudentFundContract = (providerOrSigner) =>
     new ethers.Contract(
-        import.meta.env.VITE_contract_address,
+        process.env.REACT_APP_STUDENT_FUND_CONTRACT,
         Abi,
         providerOrSigner
     );
 
 export const getFunderNft = (providerOrSigner) =>
     new ethers.Contract(
-        import.meta.env.VITE_contract_address,
+        process.env.REACT_APP_FUNDERNFT_CONTRACT,
         Abi2,
         providerOrSigner
     );
--- src/utils/constants/providers.js
+++ src/utils/constants/providers.js
@@ -2,12 +2,9 @@ import { ethers } from "ethers";
 
 // read only provider pointing to sepolia. It allows read only access to the sepolia blockchain
 export const readOnlyProvider = new ethers.JsonRpcProvider(
-    import.meta.env.VITE_rpc_url
+    process.env.REACT_APP_RPC_URL
 );
 
-export const wssProvider = new ethers.WebSocketProvider(
-    import.meta.env.VITE_wss_rpc_url
-);
 
 // read/write provider, that allows you to read data and also sign transaction on whatever chain it's pointing to
 export const getProvider = (provider) => new ethers.BrowserProvider(provider);
